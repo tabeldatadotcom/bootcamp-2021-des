9f925fbde03fe65c71c4ff9f51ec12b10dc8b239
dimasm93 <software.dimas_m@icloud.com>
2021-12-14 08:06:04 +0700

    init project

---
+++ src/main/java/com/maryanto/dimas/example/Bootcamp2021DesApplication.java
@@ -0,0 +1,13 @@
+package com.maryanto.dimas.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bootcamp2021DesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bootcamp2021DesApplication.class, args);
+	}
+
+}
