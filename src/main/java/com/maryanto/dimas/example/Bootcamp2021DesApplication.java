80924baf57a2cca19114d909c0fc3ebc6ba8ea97
dimasm93 <software.dimas_m@icloud.com>
2021-12-14 09:00:46 +0700

    init project spring dev

--- src/main/java/com/maryanto/dimas/example/Bootcamp2021DesApplication.java
+++ src/main/java/com/maryanto/dimas/example/Bootcamp2021DesApplication.java
@@ -2,12 +2,22 @@ package com.maryanto.dimas.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Bootcamp2021DesApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Bootcamp2021DesApplication.class, args);
-	}
+    @Bean
+    public String getNama() {
+        return "Dimas Maryanto";
+    }
+
+    public static void main(String[] args) {
+        ApplicationContext run = SpringApplication.run(Bootcamp2021DesApplication.class, args);
+
+        String value = run.getBean("getNama", String.class);
+        System.out.println("value: " + value);
+    }
 
 }
