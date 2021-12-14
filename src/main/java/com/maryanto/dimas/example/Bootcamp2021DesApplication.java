1ac8df216612d79f933a12c1388a5fc3646cad88
dimasm93 <software.dimas_m@icloud.com>
2021-12-14 09:27:37 +0700

    update membuat aplikasi web

--- src/main/java/com/maryanto/dimas/example/Bootcamp2021DesApplication.java
+++ src/main/java/com/maryanto/dimas/example/Bootcamp2021DesApplication.java
@@ -1,16 +1,36 @@
 package com.maryanto.dimas.example;
 
+import com.maryanto.dimas.example.model.Mahasiswa;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Bootcamp2021DesApplication {
 
     @Bean
     public String getNama() {
         return "Dimas Maryanto";
+    }
+
+    @GetMapping("/api/mahasiswa")
+    public Mahasiswa getMahasiswa() {
+        Mahasiswa mhs = new Mahasiswa();
+        mhs.setNama("Dimas Maryanto");
+        mhs.setNip("123456");
+        mhs.setKelas("IT");
+        return mhs;
+    }
+
+    @PostMapping("/api/mahasiswa")
+    public Mahasiswa getMahasiswa(@RequestBody Mahasiswa mhs) {
+        return mhs;
     }
 
     public static void main(String[] args) {
---
+++ src/main/java/com/maryanto/dimas/example/model/Mahasiswa.java
@@ -0,0 +1,11 @@
+package com.maryanto.dimas.example.model;
+
+import lombok.Data;
+
+@Data
+public class Mahasiswa {
+
+    private String nama;
+    private String nip;
+    private String kelas;
+}
